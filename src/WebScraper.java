92d9066aa484750e5837e167940ec6610f70e985
snowy-night <jilyan_dy@dlsu.edu.ph>
2022-05-20 18:51:39 +0800

    Added story scraper

--- src/WebScraper.java
+++ src/WebScraper.java
@@ -1,5 +1,36 @@
+import java.io.File;
+import java.io.PrintWriter;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
 public class WebScraper {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        final String url = "https://americanliterature.com/childrens-stories/the-three-little-pigs";
+
+        try {
+            final Document document = Jsoup.connect(url).get();
+            
+            String title = document.title();
+            
+            File dir = new File("stories/");
+            dir.mkdirs();
+            File file = new File(dir, title + ".txt");
+            file.createNewFile();
+            System.out.println(file.getAbsolutePath());
+
+            PrintWriter pw = new PrintWriter(file);
+
+            for (Element par : document.select("p,pre.childspoem")) {
+                if (!par.text().equals("")) {
+                    pw.println(par.text());
+                }
+            }
+
+            pw.close();
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
